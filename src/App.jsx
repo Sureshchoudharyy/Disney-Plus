56699a22b0a523b2dd30b1e1951b4f8c4eea5db3
Suresh Choudhary <144014929+Sureshchoudharyy@users.noreply.github.com>
2025-08-06 19:10:14 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import GenreMovieList from './Components/GenreMovieList';
+import Header from './Components/Header';
+import ProductionHouse from './Components/ProductionHouse';
+import Slider from './Components/Slider';
+
+function App(){
+  return (
+    <>
+    <div>
+      <Header />
+      <Slider />
+      <ProductionHouse/>
+      <GenreMovieList/>
+    </div>
+    </>
+  );
+}
+export default App;
